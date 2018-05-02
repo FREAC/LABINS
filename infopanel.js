e463e369a8ec07ed999845371e67a423fd3c3cdd
xiangminxufsu <cpl09c@gmail.com>
2018-05-02 15:27:54 -0400

    update templates

--- infopanel.js
+++ infopanel.js
@@ -25,12 +25,12 @@ function queryInfoPanel (results, i) {
                                 '<p><b>Description:</b> ' + results[i-1].attributes.descript + '</p>',       
                                 );
     } else if (results[i-1].attributes.layerName === 'NGS Control Points') {
-        $('#informationdiv').html('NGS Control Points: ' + results[i-1].attributes.name + '</p>' +
-                                '<p>Latitude, Longitude: ' + results[i-1].attributes.dec_lat + ', ' +  results[i-1].attributes.dec_long + '</p>' +
-                                '<p>County: ' + results[i-1].attributes.county + '</p>' + 
-                                '<p>PID: ' + results[i-1].attributes.pid + '</p>' + 
-                                '<p>Data Source: ' + '<a target="_blank" href=' + results[i-1].attributes.data_srce + '>here</a></p>' + 
-                                '<p>Datasheet: ' + '<a href=' + results[i-1].attributes.datasheet2 + '>here</a></p>',
+        $('#informationdiv').html('NGS Control Points: ' + results[i-1].attributes.NAME + '</p>' +
+                                '<p>Latitude, Longitude: ' + results[i-1].attributes.DEC_LAT + ', ' +  results[i-1].attributes.DEC_LONG + '</p>' +
+                                '<p>County: ' + results[i-1].attributes.COUNTY + '</p>' + 
+                                '<p>PID: ' + results[i-1].attributes.PID + '</p>' + 
+                                '<p>Data Source: ' + '<a target="_blank" href=' + results[i-1].attributes.DATA_SRCE + '>here</a></p>' + 
+                                '<p>Datasheet: ' + '<a href=' + results[i-1].attributes.DATASHEET2 + '>here</a></p>',
                                 );
     } else if (results[i-1].attributes.layerName === 'City Limits') {
         $('#informationdiv').html('<p><b>City limits:</b> ' + results[i-1].attributes.name + '</p>' +

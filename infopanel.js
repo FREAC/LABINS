235fc2b51de12d471670ea5a3dbc62308bc050f7
Clinton Lunn <clintonlunn@Clintons-MacBook-Pro.local>
2019-09-18 22:11:38 -0400

    remove streetview links from everywhere except top of info panel

--- infopanel.js
+++ infopanel.js
@@ -61,8 +61,7 @@ async function queryInfoPanel(event, results, i) {
                     'Latitude, Longitude: ' + results[i - 1].attributes.dec_lat + ', ' + results[i - 1].attributes.dec_long + '<br>' +
                     'County: ' + results[i - 1].attributes.county + '<br>' +
                     'PID: ' + results[i - 1].attributes.pid + '<br>' +
-                    'Datasheet: ' + '<a target="_blank" href=' + results[i - 1].attributes.data_srce + '>' + results[i - 1].attributes.pid + '</a><br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a><br>'
+                    'Datasheet: ' + '<a target="_blank" href=' + results[i - 1].attributes.data_srce + '>' + results[i - 1].attributes.pid + '</a><br>'
                 );
 
                 // https://medium.com/netscape/hacking-it-out-when-cors-wont-let-you-be-great-35f6206cc646
@@ -90,8 +89,7 @@ async function queryInfoPanel(event, results, i) {
                     'Latitude, Longitude: ' + results[i - 1].attributes.dec_lat + ', ' + results[i - 1].attributes.dec_long + '<br>' +
                     'County: ' + results[i - 1].attributes.county + '<br>' +
                     'PID: ' + results[i - 1].attributes.pid + '<br>' +
-                    'Datasheet: ' + '<a target="_blank" href=' + results[i - 1].attributes.datasheet2 + '>' + results[i - 1].attributes.pid + '</a><br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a><br>'
+                    'Datasheet: ' + '<a target="_blank" href=' + results[i - 1].attributes.datasheet2 + '>' + results[i - 1].attributes.pid + '</a><br>'
                 );
             } else if (results[i - 1].attributes.layerName === 'City Limits') {
                 $('#informationdiv').append('<p style= "font-size: 15px"><b>City Limits</b></p>' +
@@ -114,9 +112,7 @@ async function queryInfoPanel(event, results, i) {
                     '<b>Latitude: </b>' + results[i - 1].attributes.latdecdeg + '<br>' +
                     '<b>Longitude: </b>' + results[i - 1].attributes.londecdeg + '<br>' +
                     'Abstract: ' + '<a href=' + results[i - 1].attributes.abstract + '>' + results[i - 1].attributes.l_number + '</a><br>',
-                    'Description: ' + '<a href=' + results[i - 1].attributes.description2 + '>' + results[i - 1].attributes.l_number + '</a><br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a>'
-
+                    'Description: ' + '<a href=' + results[i - 1].attributes.description2 + '>' + results[i - 1].attributes.l_number + '</a><br>'
                 );
             } else if (results[i - 1].attributes.layerName === 'Tide Stations') {
                 $('#informationdiv').append('<p style= "font-size: 15px"><b>Tide Stations</b></p>' +
@@ -127,9 +123,7 @@ async function queryInfoPanel(event, results, i) {
                     '<b>Status: </b>' + results[i - 1].attributes.status + '<br>' +
                     '<b>MHW (feet): </b>' + results[i - 1].attributes.navd88mhw_ft + '<br>' +
                     '<b>MLW (feet): </b>' + results[i - 1].attributes.navd88mlw_ft + '<br>' +
-                    "<b>Steven's ID: </b>" + results[i - 1].attributes.stevens_id + '<br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a><br>'
-
+                    "<b>Steven's ID: </b>" + results[i - 1].attributes.stevens_id + '<br>'
                 );
                 // Do not include link to DEP report if the old link is present
                 // if (results[i - 1].attributes.report_dep.substring(0, 36) == 'ftp://ftp.labins.org/tide/NewReports') {
@@ -154,8 +148,7 @@ async function queryInfoPanel(event, results, i) {
                     // '<b>MHW (feet): </b>' + results[i - 1].attributes.mhw2_ft + '<br>' +
                     // '<b>MLW (feet): </b>' + results[i - 1].attributes.mlw2_ft + '<br>' +
                     '<b>Station 1: </b>' + results[i - 1].attributes.station1 + '<br>' +
-                    '<b>Station 2: </b>' + results[i - 1].attributes.station2 + '<br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a><br>'
+                    '<b>Station 2: </b>' + results[i - 1].attributes.station2 + '<br>'
                 );
                 if (results[i - 1].attributes.status_col === "1") {
                     // This is not a tidal point
@@ -173,8 +166,7 @@ async function queryInfoPanel(event, results, i) {
                     '<b>State Plane Zone: </b>' + results[i - 1].attributes.state_plane_zone + '<br>' +
                     '<b>County: </b>' + results[i - 1].attributes.county + '<br>' +
                     '<b>Latitude: </b>' + results[i - 1].attributes.latitude + '<br>' +
-                    '<b>Longitude: </b>' + results[i - 1].attributes.longitude + '<br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a>'
+                    '<b>Longitude: </b>' + results[i - 1].attributes.longitude + '<br>'
                 );
             } else if (results[i - 1].attributes.layerName === 'Erosion Control Line') {
                 $('#informationdiv').append('<p style= "font-size: 15px"><b>Erosion Control Line</b></p>' +
@@ -188,8 +180,7 @@ async function queryInfoPanel(event, results, i) {
                 var replaceWhitespace = results[i - 1].attributes.FILE_NAME.replace(" ", "%20");
                 $('#informationdiv').append('<p style= "font-size: 15px"><b>SWFWMD Survey Benchmarks</b></p>' +
                     '<b>Benchmark Name: </b>' + results[i - 1].attributes.BENCHMARK_NAME + '<br>' +
-                    'More Information: </b><a target="_blank" href=http://ftp.labins.org/swfwmd/SWFWMD_control_2013/' + replaceWhitespace + '>' + results[i - 1].attributes.FILE_NAME + '</a><br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a><br>  '
+                    'More Information: </b><a target="_blank" href=http://ftp.labins.org/swfwmd/SWFWMD_control_2013/' + replaceWhitespace + '>' + results[i - 1].attributes.FILE_NAME + '</a><br>'
                 );
 
             } else if (results[i - 1].attributes.layerName === 'Township-Range-Section') {
@@ -205,9 +196,7 @@ async function queryInfoPanel(event, results, i) {
             } else if (results[i - 1].attributes.layerName === 'Geographic Names') {
                 $('#informationdiv').append('<p style= "font-size: 15px"><b>Geographic Names</b> </p>' +
                     '<b>Feature Name: </b>' + results[i - 1].attributes.feature_na + '<br>' +
-                    '<b>Feature Class: </b>' + results[i - 1].attributes.feature_cl + '<br>' +
-                    '<a target="_blank" href=http://maps.google.com/maps?q=&layer=c&cbll=' + results[i - 1].geometry.latitude + ',' + results[i - 1].geometry.longitude + '>Google Street View</a>'
-
+                    '<b>Feature Class: </b>' + results[i - 1].attributes.feature_cl + '<br>'
                 );
 
             } else if (results[i - 1].attributes.layerName === 'Certified Corners' && results[i - 1].attributes.is_image === 'Y') {

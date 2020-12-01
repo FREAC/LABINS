7fa06c2faa310fc1ae78c24d07410870e7ca8609
Stephen Hodge <shodge@fsu.edu>
2020-12-01 15:25:43 -0500

    sort needed to be in reverse order

--- infopanel.js
+++ infopanel.js
@@ -234,7 +234,7 @@ async function queryInfoPanel(event = false, results, i) {
 
                 // const relatedFeatures = await results[i - 1].attributes.relatedFeatures;
                 console.log(results[i - 1].attributes);
-                const relatedFeatures = results[i - 1].attributes.relatedFeatures.sort(function(a, b) {return a-b});
+                const relatedFeatures = results[i - 1].attributes.relatedFeatures.sort(function(a, b) {return b-a});
                 console.log('after the sort',relatedFeatures);
 
                 for (relatedFeature in relatedFeatures) {
